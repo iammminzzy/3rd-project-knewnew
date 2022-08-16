7d41c403e7814b95615b9c87c95b15600292ec52
doll8721 <doll8721@gmail.com>
2022-08-16 17:34:08 +0900

    초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import { Router } from './Router';
 import GlobalStyle from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 
 function App() {
+  // * react query 글로벌 에러 핸들링 세팅
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: error => {
+        console.log(error);
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: error => {
+        console.log(error);
+      },
+    }),
+  });
+
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Router />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
