8dab62eecf925cff66943759566b6150628a09e3
hoonjo1 <hoonjo1@gmail.com>
2022-08-19 14:31:08 +0900

    Router 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+
+import Home from './pages/Home/Home';
+import NewFeed from './pages/NewFeed/NewFeed';
+import Search from './pages/Search/Search';
+import Users from './pages/Users/Users';
+import Detail from './pages/Detail/Detail';
+import NotFound from './pages/NotFound/NotFound';
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/newfeed" element={<NewFeed />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/Detail/:id" element={<Detail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
