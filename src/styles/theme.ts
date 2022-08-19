f0df0a6885ad85746a95becf1f034432ceb72af0
hoonjo1 <hoonjo1@gmail.com>
2022-08-19 10:48:13 +0900

    theme 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,16 @@
 const color = {
-  white: 'white',
-  black: 'black',
+  white: '#FFFFFF',
+  white80: '#F7F7FC',
+  white50: '#ddd',
+
+  black: '#000000',
+  black80: '#202124',
+  black50: '#AAA',
+
+  red: '#FB585A',
+  red80: '#FE5D5D',
+
+  yello: '#f5df4d',
 };
 
 export const theme = {
